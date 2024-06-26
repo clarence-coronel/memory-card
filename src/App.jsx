de0ff9ee75299bfea5520c58ee4b7a877599e533
Clarence Coronel <clarence.coronel.r@gmail.com>
2024-06-26 11:34:19 +0800

    feat: add game mechanic

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ export default function App() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [agentPool, setAgentPool] = useState(null);
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  const [insertedAgents, setInsertedAgents] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,14 +20,15 @@ export default function App() {
 
         const fetchedData = await response.json();
 
-        setData(
-          fetchedData.data.filter(
-            (agent) =>
-              agent.displayName && agent.fullPortrait && agent.background
-          )
+        const filteredData = fetchedData.data.filter(
+          (agent) => agent.displayName && agent.fullPortrait && agent.background
         );
 
-        console.log(fetchedData.data);
+        setData(filteredData);
+
+        setAgentPool(generateNewAgentPool(filteredData, 12));
+
+        // console.log(fetchedData.data);
       } catch (error) {
         setError(error);
       } finally {
@@ -32,20 +37,79 @@ export default function App() {
     };
 
     fetchData();
+
+    console.log(agentPool);
   }, []);
 
+  const generateNewAgentPool = (agents, limit) => {
+    const shuffledAgents = shuffleArray(agents);
+
+    return shuffledAgents.slice(0, limit);
+  };
+
+  const reloadAgentPool = () => {
+    setAgentPool(generateNewAgentPool(data, 12));
+  };
+
+  const shuffleArray = (array) => {
+    let currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex !== 0) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  };
+
+  const cardClickHandler = (displayName) => {
+    reloadAgentPool();
+
+    if (!insertedAgents.find((agentName) => agentName === displayName)) {
+      setScore(score + 1);
+      setInsertedAgents([...insertedAgents, displayName]);
+    } else {
+      if (score > highScore) setHighScore(score);
+
+      setScore(0);
+      setInsertedAgents([]);
+    }
+  };
+
   return (
-    <div className="grid grid-cols-3 gap-5">
-      {!loading &&
-        data &&
-        data.map((agent) => (
-          <Card
-            key={agent.uuid}
-            img={agent.fullPortrait}
-            background={agent.background}
-            displayName={agent.displayName}
-          />
-        ))}
+    <div className="min-h-screen w-full bg-neutral-900">
+      <div className="bg-neutral-800 text-white p-2 flex justify-center items-center gap-5">
+        <h1 className="flex justify-center items-center gap-1">
+          <span>Score: </span>
+          <span className="text-red-400 text-xl">{score}</span>
+        </h1>
+        <h2>
+          <span>Highest Score: </span>
+          <span className="text-red-400 text-xl">{highScore}</span>
+        </h2>
+      </div>
+      <div className="max-md:grid-cols-2 max-xl:grid-cols-4 grid grid-cols-6 gap-5 p-5">
+        {!loading &&
+          data &&
+          agentPool.map((agent) => (
+            <Card
+              key={agent.uuid}
+              img={agent.fullPortrait}
+              background={agent.background}
+              displayName={agent.displayName}
+              action={cardClickHandler}
+            />
+          ))}
+      </div>
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,21 @@
-export default function Card({ img, background, displayName }) {
+export default function Card({ img, background, displayName, action }) {
   return (
-    <div className="max-w-[300px] mx-auto rounded-md overflow-hidden">
-      <div className="hover:bg-red-500 hover:cursor-pointer duration-200 group relative bg-red-400">
+    <button
+      onClick={() => action(displayName)}
+      className="max-w-[400px] mx-auto rounded-lg overflow-hidden"
+    >
+      <div className="hover:bg-red-500 rounded-lg p-2 hover:cursor-pointer duration-200 group relative bg-red-400">
         <img
-          className="group-hover:scale-90 duration-500 z-10 w-full h-full left-0 top-0 absolute object-contain"
+          className="group-hover:scale-75 duration-500 z-10 w-full h-full left-0 top-0 absolute object-contain"
           src={background}
           alt="background"
         />
         <img
-          className="aspect-[2/3] group-hover:scale-125 duration-500 z-50 relative object-contain"
+          className="aspect-[2/3] group-hover:scale-150 duration-500 z-50 relative object-contain"
           src={img}
           alt={displayName}
         />
       </div>
-    </div>
+    </button>
   );
 }
